c5a16cf336ed2589b7cbb1a43a2c8a3e23587f36
alphasp <gmerudotcom@gmail.com>
2016-12-09 13:58:34 +0800

    added leak canary for android

--- android/app/src/main/java/com/pixiv/MainApplication.java
+++ android/app/src/main/java/com/pixiv/MainApplication.java
@@ -3,6 +3,7 @@ package com.pixiv;
 import android.app.Application;
 import android.util.Log;
 
+import com.squareup.leakcanary.LeakCanary;
 import com.facebook.react.ReactApplication;
 import com.crashlytics.android.Crashlytics;
 import io.fabric.sdk.android.Fabric;
@@ -59,5 +60,11 @@ public class MainApplication extends Application implements ReactApplication {
     super.onCreate();
     SoLoader.init(this, /* native exopackage */ false);
     Fabric.with(this, new Crashlytics());
+    if (LeakCanary.isInAnalyzerProcess(this)) {
+      // This process is dedicated to LeakCanary for heap analysis.
+      // You should not init your app in this process.
+      return;
+    }
+    LeakCanary.install(this);
   }
 }
