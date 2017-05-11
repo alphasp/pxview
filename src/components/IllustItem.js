de44bccd6433da9f3212967171cd14c81ee30d05
alphasp <gmerudotcom@gmail.com>
2017-05-11 18:17:27 +0800

    apply flex grow and better margin on illust item

--- src/components/IllustItem.js
+++ src/components/IllustItem.js
@@ -1,11 +1,9 @@
 import React, { Component } from 'react';
-import { Dimensions } from 'react-native';
 import PXTouchable from './PXTouchable';
 import PXImage from './PXImage';
 import OverlayImagePages from './OverlayImagePages';
 import OverlayBookmarkButton from '../components/OverlayBookmarkButton';
-
-const width = Dimensions.get('window').width; // full width
+import { globalStyleVariables } from '../styles';
 
 class IllustItem extends Component {
   shouldComponentUpdate(nextProps) {
@@ -19,26 +17,36 @@ class IllustItem extends Component {
   }
 
   render() {
-    const { item, onPressItem } = this.props;
-    // console.log('render ', item.id)
+    const {
+      item,
+      index,
+      numColumns,
+      onPressItem,
+      containerStyle,
+      imageStyle,
+    } = this.props;
     return (
       <PXTouchable
-        style={{
-          margin: 1,
-          backgroundColor: '#E9EBEE',
-          width: width / 3 - 2,
-          height: width / 3 - 2,
-        }}
-        key={item.id}
+        style={[
+          {
+            marginRight: index % numColumns < numColumns - 1 ? 1 : 0,
+            marginBottom: 1,
+            backgroundColor: globalStyleVariables.BACKGROUND_COLOR,
+            flexGrow: 1,
+          },
+          containerStyle,
+        ]}
         onPress={onPressItem}
       >
         <PXImage
           uri={item.image_urls.square_medium}
-          style={{
-            width: width / 3 - 2,
-            height: width / 3 - 2,
-            resizeMode: 'cover',
-          }}
+          style={[
+            {
+              height: globalStyleVariables.WINDOW_WIDTH / numColumns,
+              resizeMode: 'cover',
+            },
+            imageStyle,
+          ]}
         />
         {item.meta_pages && item.meta_pages.length
           ? <OverlayImagePages total={item.meta_pages.length} />
