ab620f53d7b4bfa6478c19666a892346f6c27ddb
alphasp <gmerudotcom@gmail.com>
2017-06-08 17:54:56 +0800

    added HeaderFilterButton

---
+++ src/components/HeaderFilterButton.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import PXTouchable from './PXTouchable';
+
+const styles = StyleSheet.create({
+  icon: {
+    padding: 10,
+  },
+});
+
+const HeaderFilterButton = ({ onPress, color, ...restProps }) => (
+  <PXTouchable onPress={onPress} {...restProps}>
+    <Icon
+      name="sliders"
+      size={20}
+      style={styles.icon}
+      color={color || '#fff'}
+    />
+  </PXTouchable>
+);
+
+export default HeaderFilterButton;
