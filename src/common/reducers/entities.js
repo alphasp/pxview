6de8d4aff3485de9df023a2c143fea9cf812ffef
alphasp <gmerudotcom@gmail.com>
2017-06-17 21:51:16 +0800

    fixed follow button in user detail not working properly

--- src/common/reducers/entities.js
+++ src/common/reducers/entities.js
@@ -78,6 +78,18 @@ export default function entities(
             is_followed: true,
           },
         },
+        userProfiles: state.userProfiles[action.payload.userId]
+          ? {
+              ...state.userProfiles,
+              [action.payload.userId]: {
+                ...state.userProfiles[action.payload.userId],
+                user: {
+                  ...state.userProfiles[action.payload.userId].user,
+                  is_followed: true,
+                },
+              },
+            }
+          : state.userProfiles,
       };
     case UNFOLLOW_USER.REQUEST:
       if (!state.users[action.payload.userId]) {
@@ -92,6 +104,18 @@ export default function entities(
             is_followed: false,
           },
         },
+        userProfiles: state.userProfiles[action.payload.userId]
+          ? {
+              ...state.userProfiles,
+              [action.payload.userId]: {
+                ...state.userProfiles[action.payload.userId],
+                user: {
+                  ...state.userProfiles[action.payload.userId].user,
+                  is_followed: false,
+                },
+              },
+            }
+          : state.userProfiles,
       };
     default:
       return state;
