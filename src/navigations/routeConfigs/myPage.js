76a1a6327cad9b878ee2eab71bd2aed26573e628
alphasp <gmerudotcom@gmail.com>
2017-06-22 16:11:48 +0800

    remove header shadow on pages with top tab bar

--- src/navigations/routeConfigs/myPage.js
+++ src/navigations/routeConfigs/myPage.js
@@ -6,24 +6,37 @@ import Settings from '../../screens/MyPage/Settings';
 import AccountSettings from '../../screens/MyPage/AccountSettings';
 import Language from '../../screens/MyPage/Language';
 import Feedback from '../../screens/MyPage/Feedback';
+import { globalStyleVariables } from '../../styles';
+
+const headerWithShadowStyle = {
+  shadowOpacity: 0,
+  shadowOffset: {
+    height: 0,
+  },
+  elevation: 0,
+  backgroundColor: globalStyleVariables.HEADER_BACKGROUND_COLOR,
+};
 
 const config = {
   MyConnection: {
     screen: MyConnection,
     navigationOptions: ({ screenProps: { i18n } }) => ({
       title: i18n.connection,
+      headerStyle: headerWithShadowStyle,
     }),
   },
   MyCollection: {
     screen: MyCollection,
     navigationOptions: ({ screenProps: { i18n } }) => ({
       title: i18n.collection,
+      headerStyle: headerWithShadowStyle,
     }),
   },
   MyWorks: {
     screen: MyWorks,
     navigationOptions: ({ screenProps: { i18n } }) => ({
       title: i18n.myWorks,
+      headerStyle: headerWithShadowStyle,
     }),
   },
   BrowsingHistory: {
