be0d434586cbef6a13f90c019ce45b2879884c68
alphasp <gmerudotcom@gmail.com>
2017-07-03 16:50:02 +0800

    removed unused files

--- getFlatList.bash
+++
@@ -1,6 +0,0 @@
-mkdir node_modules/react-native/Libraries/CustomComponents
-mkdir node_modules/react-native/Libraries/CustomComponents/Lists
-for file in 'FlatList' 'MetroListView' 'SectionList' 'VirtualizedList' 'VirtualizeUtils' 'ViewabilityHelper' 'VirtualizedSectionList'; \
-  do curl https://raw.githubusercontent.com/facebook/react-native/master/Libraries/CustomComponents/Lists/${file}.js > node_modules/react-native/Libraries/CustomComponents/Lists/${file}.js; \
-  done
-  Libraries/CustomComponents/Lists/SectionList.js 
