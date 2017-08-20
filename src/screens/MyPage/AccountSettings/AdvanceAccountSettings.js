578f0e091f645ac5fb4c46562185626461ba6b47
alphasp <gmerudotcom@gmail.com>
2017-08-20 22:53:15 +0800

    added PXWebView

--- src/screens/MyPage/AccountSettings/AdvanceAccountSettings.js
+++ src/screens/MyPage/AccountSettings/AdvanceAccountSettings.js
@@ -1,64 +1,13 @@
-import React, { Component } from 'react';
-import { View, WebView } from 'react-native';
-import ProgressBar from 'react-native-progress/Bar';
-import Loader from '../../../components/Loader';
-import { globalStyles, globalStyleVariables } from '../../../styles';
+import React from 'react';
+import PXWebView from '../../../components/PXWebView';
 
 const SETTING_URL = 'https://touch.pixiv.net/setting_user.php';
 
-class AdvanceAccountSettings extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      loading: true,
-      loadedOnce: false,
-    };
-  }
-
-  handleOnLoadStart = () => {
-    this.setState({
-      loading: true,
-    });
-  };
-
-  handleOnLoadEnd = () => {
-    const { loadedOnce } = this.state;
-    const newState = {
-      loading: false,
-    };
-    if (!loadedOnce) {
-      newState.loadedOnce = true;
-    }
-    this.setState(newState);
-  };
-
-  renderLoader = () => <Loader />;
-
-  render() {
-    const { loadedOnce, loading } = this.state;
-    return (
-      <View style={globalStyles.container}>
-        {loadedOnce &&
-          loading &&
-          <ProgressBar
-            indeterminate
-            borderRadius={0}
-            width={globalStyleVariables.WINDOW_WIDTH}
-            height={3}
-          />}
-        <WebView
-          source={{
-            uri: SETTING_URL,
-          }}
-          renderLoading={this.renderLoader}
-          onLoadStart={this.handleOnLoadStart}
-          onLoadEnd={this.handleOnLoadEnd}
-          startInLoadingState
-          javaScriptEnabled
-        />
-      </View>
-    );
-  }
-}
+const AdvanceAccountSettings = () =>
+  <PXWebView
+    source={{
+      uri: SETTING_URL,
+    }}
+  />;
 
 export default AdvanceAccountSettings;
