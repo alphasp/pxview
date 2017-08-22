844c934fb4db215edd87fe0c5ea8d82356e58aa1
alphasp <gmerudotcom@gmail.com>
2017-08-22 17:10:19 +0800

    added PXBottomSheetButton, PXButtomSheetCancelButton

---
+++ src/components/PXBottomSheetCancelButton.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+import IonicIcon from 'react-native-vector-icons/Ionicons';
+import PXTouchable from './PXTouchable';
+
+const styles = StyleSheet.create({
+  bottomSheetListItem: {
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    height: 48,
+  },
+  bottomSheetCancelIcon: {
+    marginLeft: 3,
+  },
+  bottomSheetCancelText: {
+    marginLeft: 36,
+  },
+});
+
+const PXBottomSheetCancelButton = ({ onPress, text }) =>
+  <PXTouchable onPress={onPress}>
+    <View style={styles.bottomSheetListItem}>
+      <IonicIcon
+        name="md-close"
+        size={24}
+        style={styles.bottomSheetCancelIcon}
+      />
+      <Text style={styles.bottomSheetCancelText}>
+        {text}
+      </Text>
+    </View>
+  </PXTouchable>;
+
+export default PXBottomSheetCancelButton;
