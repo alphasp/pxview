7fe6270efa9030a12e5350ef699ca105d7804e8c
alphasp <gmerudotcom@gmail.com>
2017-09-28 19:24:19 +0800

    update android package identifier for react native splash screen

--- android/app/src/main/java/com/utopia/pxview/MainActivity.java
+++ android/app/src/main/java/com/utopia/pxview/MainActivity.java
@@ -2,7 +2,7 @@ package com.utopia.pxview;
 
 import android.os.Bundle;
 import com.facebook.react.ReactActivity;
-import com.cboy.rn.splashscreen.SplashScreen; 
+import org.devio.rn.splashscreen.SplashScreen; 
 
 public class MainActivity extends ReactActivity {
 
