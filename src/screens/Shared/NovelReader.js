f834bb9cb50ad83ae8090fcb07a17da1ac78c922
alphasp <gmerudotcom@gmail.com>
2017-12-19 18:58:28 +0800

    extract novel viewer as component from novel reader

--- src/screens/Shared/NovelReader.js
+++ src/screens/Shared/NovelReader.js
@@ -1,18 +1,8 @@
 import React, { Component } from 'react';
-import {
-  View,
-  Platform,
-  StatusBar,
-  StyleSheet,
-  ScrollView,
-  Text,
-  InteractionManager,
-} from 'react-native';
+import { View, InteractionManager } from 'react-native';
 import { connect } from 'react-redux';
-import HtmlView from 'react-native-htmlview';
-import { connectLocalization } from '../../components/Localization';
+import NovelViewer from '../../components/NovelViewer';
 import PXHeader from '../../components/PXHeader';
-import PXViewPager from '../../components/PXViewPager';
 import HeaderTextTitle from '../../components/HeaderTextTitle';
 import HeaderSettingsButton from '../../components/HeaderSettingsButton';
 import Loader from '../../components/Loader';
@@ -20,23 +10,7 @@ import * as novelTextActionCreators from '../../common/actions/novelText';
 import * as modalActionCreators from '../../common/actions/modal';
 import { makeGetParsedNovelText } from '../../common/selectors';
 import { MODAL_TYPES } from '../../common/constants';
-import { globalStyles, globalStyleVariables } from '../../styles';
-
-const styles = StyleSheet.create({
-  container: {
-    width: globalStyleVariables.WINDOW_WIDTH,
-    padding: 10,
-  },
-  novelChapter: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    marginBottom: 10,
-  },
-  pageLink: {
-    fontWeight: '500',
-    color: '#007AFF',
-  },
-});
+import { globalStyles } from '../../styles';
 
 class NovelReader extends Component {
   constructor(props) {
@@ -56,46 +30,12 @@ class NovelReader extends Component {
     }
   }
 
-  handleOnViewPagerPageSelected = index => {
-    if (this.state.index !== index) {
-      this.setState({
-        index,
-      });
-    }
+  handleOnIndexChange = index => {
+    this.setState({ index });
   };
 
-  handleRenderNode = (node, index, siblings, parent, defaultRenderer) => {
-    if (node.name === 'chapter') {
-      return (
-        <Text key={index} style={styles.novelChapter}>
-          {defaultRenderer(node.children, parent)}
-        </Text>
-      );
-    } else if (node.name === 'jump') {
-      const { page } = node.attribs;
-      return (
-        <Text
-          key={index}
-          style={styles.pageLink}
-          onPress={() => this.handleOnPressNovelPageLink(page)}
-        >
-          {defaultRenderer(node.children, parent)}
-        </Text>
-      );
-    }
-  };
-
-  handleOnPressNovelPageLink = page => {
-    console.log('this.viewpager ', this.viewPager);
-    if (this.viewPager) {
-      if (Platform.OS === 'android') {
-        this.viewPager.setPage(page - 1);
-      } else if (Platform.OS === 'ios') {
-        this.viewPager.scrollToIndex({
-          index: page - 1,
-        });
-      }
-    }
+  handleOnPressPageLink = page => {
+    this.setState({ index: parseInt(page, 10) - 1 });
   };
 
   handleOnPressOpenSettings = () => {
@@ -114,42 +54,17 @@ class NovelReader extends Component {
   };
 
   renderHeaderRight = () =>
-    <View style={styles.headerRightContainer}>
-      <HeaderSettingsButton onPress={this.handleOnPressOpenSettings} />
-    </View>;
-
-  renderContent = ({ item, index }) => {
-    const { novelId, novelSettings: { fontSize, lineHeight } } = this.props;
-    // render text by chunks to prevent over text limit https://github.com/facebook/react-native/issues/15663
-    return (
-      <View
-        key={`${novelId}-${index}-${fontSize}-${lineHeight}`}
-        style={styles.container}
-      >
-        <ScrollView>
-          {item.match(/.{1,5000}/g).map((t, i) =>
-            <HtmlView
-              key={`${novelId}-${index}-${i}`}
-              value={t}
-              renderNode={this.handleRenderNode}
-              textComponentProps={{
-                style: {
-                  fontSize,
-                  lineHeight: fontSize * lineHeight,
-                },
-              }}
-            />,
-          )}
-          <View style={{ marginBottom: 50 }} />
-        </ScrollView>
-      </View>
-    );
-  };
+    <HeaderSettingsButton onPress={this.handleOnPressOpenSettings} />;
 
   render() {
-    const { novelId, novelText, parsedNovelText } = this.props;
+    const {
+      novelId,
+      novelText,
+      parsedNovelText,
+      novelSettings: { fontSize, lineHeight },
+    } = this.props;
     const { index } = this.state;
-    console.log('novel ', novelId, parsedNovelText, novelText, index);
+    // console.log('novel ', novelId, parsedNovelText, novelText, index);
     return (
       <View style={globalStyles.container}>
         <PXHeader
@@ -160,35 +75,34 @@ class NovelReader extends Component {
         />
         {(!novelText || (!novelText.loaded || novelText.loading)) && <Loader />}
         {parsedNovelText &&
-          <PXViewPager
-            viewPagerRef={ref => (this.viewPager = ref)}
+          <NovelViewer
+            novelId={novelId}
             items={parsedNovelText}
-            keyExtractor={(item, i) => `${novelId}-${i}`}
             index={index}
-            renderContent={this.renderContent}
-            onPageSelected={this.handleOnViewPagerPageSelected}
+            fontSize={fontSize}
+            lineHeight={lineHeight}
+            onIndexChange={this.handleOnIndexChange}
+            onPressPageLink={this.handleOnPressPageLink}
           />}
       </View>
     );
   }
 }
 
-export default connectLocalization(
-  connect(
-    () => {
-      const getParsedNovelText = makeGetParsedNovelText();
-      return (state, props) => {
-        const { novelText, novelSettings } = state;
-        const parsedNovelText = getParsedNovelText(state, props);
-        const novelId = props.novelId || props.navigation.state.params.novelId;
-        return {
-          novelText: novelText[novelId],
-          novelId,
-          parsedNovelText,
-          novelSettings,
-        };
+export default connect(
+  () => {
+    const getParsedNovelText = makeGetParsedNovelText();
+    return (state, props) => {
+      const { novelText, novelSettings } = state;
+      const parsedNovelText = getParsedNovelText(state, props);
+      const novelId = props.novelId || props.navigation.state.params.novelId;
+      return {
+        novelText: novelText[novelId],
+        novelId,
+        parsedNovelText,
+        novelSettings,
       };
-    },
-    { ...novelTextActionCreators, ...modalActionCreators },
-  )(NovelReader),
-);
+    };
+  },
+  { ...novelTextActionCreators, ...modalActionCreators },
+)(NovelReader);
