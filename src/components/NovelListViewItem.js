9bd94bdc0d2911d3b40d6f194b94aea8579cb7b7
alphasp <gmerudotcom@gmail.com>
2018-01-23 15:59:29 +0800

    added overlay muted indicator to novel list view item

--- src/components/NovelListViewItem.js
+++ src/components/NovelListViewItem.js
@@ -4,7 +4,7 @@ import PXTouchable from './PXTouchable';
 import PXImage from './PXImage';
 import OverlayBookmarkNovelButton from './OverlayBookmarkNovelButton';
 import OverlayNovelPages from './OverlayNovelPages';
-// import OverlayMutedIndicator from '../components/OverlayMutedIndicator';
+import OverlayMutedIndicator from './OverlayMutedIndicator';
 import { globalStyleVariables } from '../styles';
 
 const HIGHLIGHT_BORDER_WIDTH = 3;
@@ -13,7 +13,17 @@ const styles = StyleSheet.create({
     marginHorizontal: 10,
     marginBottom: 10,
     backgroundColor: '#fff',
+  },
+  contentContainer: {
     flexDirection: 'row',
+  },
+  muteContainer: {
+    height: 120,
+    justifyContent: 'center',
+  },
+  imageContainer: {
+    width: 75,
+    height: 100,
   },
   highlight: {
     borderWidth: HIGHLIGHT_BORDER_WIDTH,
@@ -79,44 +89,50 @@ class NovelListViewItem extends Component {
         onPress={onPressItem}
         disabled={isMute}
       >
-        <View style={{ width: 75, height: 100 }}>
-          <PXImage
-            uri={item.image_urls.square_medium}
-            style={[
-              {
-                resizeMode: 'cover',
-                flex: 1,
-              },
-              imageStyle,
-            ]}
-          />
-          {
-            <OverlayBookmarkNovelButton
-              item={item}
-              total={item.total_bookmarks}
-            />
-          }
-          {item.page_count > 1
-            ? <OverlayNovelPages total={item.page_count} />
-            : null}
-        </View>
-        <View style={styles.subRightContainer}>
-          {item.series &&
-            item.series.id &&
-            <Text style={[styles.text, styles.seriesTitle]}>
-              {item.series.title}
-            </Text>}
-          <Text style={[styles.text, styles.title]} ellipsisMode="tail">
-            {item.title}
-          </Text>
-          <Text style={[styles.text, styles.user]}>
-            by {item.user.name}
-          </Text>
-          <Text style={[styles.text, styles.info]}>
-            {item.text_length}words
-          </Text>
-          {this.renderTags(item.tags)}
-        </View>
+        {isMute
+          ? <View style={styles.muteContainer}>
+              <OverlayMutedIndicator />
+            </View>
+          : <View style={styles.contentContainer}>
+              <View style={styles.imageContainer}>
+                <PXImage
+                  uri={item.image_urls.square_medium}
+                  style={[
+                    {
+                      resizeMode: 'cover',
+                      flex: 1,
+                    },
+                    imageStyle,
+                  ]}
+                />
+                {
+                  <OverlayBookmarkNovelButton
+                    item={item}
+                    total={item.total_bookmarks}
+                  />
+                }
+                {item.page_count > 1
+                  ? <OverlayNovelPages total={item.page_count} />
+                  : null}
+              </View>
+              <View style={styles.subRightContainer}>
+                {item.series &&
+                  item.series.id &&
+                  <Text style={[styles.text, styles.seriesTitle]}>
+                    {item.series.title}
+                  </Text>}
+                <Text style={[styles.text, styles.title]} ellipsisMode="tail">
+                  {item.title}
+                </Text>
+                <Text style={[styles.text, styles.user]}>
+                  by {item.user.name}
+                </Text>
+                <Text style={[styles.text, styles.info]}>
+                  {item.text_length}words
+                </Text>
+                {this.renderTags(item.tags)}
+              </View>
+            </View>}
       </PXTouchable>
     );
   }
