c116994104da41b534fc74a60b7909d5e9e08ba8
alphasp <gmerudotcom@gmail.com>
2018-03-20 17:13:13 +0800

    move novelTextParser as helper

---
+++ src/common/helpers/novelTextParser.js
@@ -0,0 +1,55 @@
+import { Parser as NovelParser } from 'pixiv-novel-parser';
+
+const parseNovelText = novelText => {
+  // const parsedNovelText = NovelParser.parse(
+  //   `${novelText
+  //     .text}[jump:2]blabla[[jumpuri:とある[[rb: 魔術 > まじゅつ]]の[[rb:禁書目録>インデックス]] > http://www.project-index.net/]]`,
+  // );
+  const parsedNovelText = NovelParser.parse(novelText);
+  const items = [];
+  let text = '';
+  parsedNovelText.forEach((p, index) => {
+    if (p.type === 'text') {
+      text += p.val;
+    } else if (p.type === 'tag') {
+      if (p.name === 'chapter') {
+        text += '<chapter>';
+        p.title.forEach(pp => {
+          if (pp.name === 'text') {
+            text += pp.val;
+          } else if (pp.name === 'rb') {
+            text += `${pp.rubyBase}(${pp.rubyText})`;
+          }
+        });
+        text += '</chapter>';
+      } else if (p.name === 'rb') {
+        text += `${p.rubyBase}(${p.rubyText})`;
+      } else if (p.name === 'jump') {
+        text += `<jump page=${p.pageNumber}>${p.pageNumber}ページへ</jump>`;
+      } else if (p.name === 'jumpuri') {
+        text += `<a href='${p.uri}'>`;
+        p.title.forEach(pp => {
+          if (pp.type === 'text') {
+            text += pp.val;
+          } else if (pp.type === 'rb') {
+            text += `${pp.rubyBase}(${pp.rubyText})`;
+          }
+        });
+        text += '</a>';
+      } else if (p.name === 'newpage') {
+        items.push(text);
+        text = '';
+      }
+    }
+    // if this is last item and no new page tag
+    if (
+      index === parsedNovelText.length - 1 &&
+      (!items.length || p.name !== 'newpage')
+    ) {
+      items.push(text);
+    }
+  });
+  return items;
+};
+
+export default parseNovelText;
--- src/common/selectors/index.js
+++ src/common/selectors/index.js
@@ -4,7 +4,7 @@
 import { createSelector, createSelectorCreator } from 'reselect';
 import equals from 'shallow-equals';
 import { denormalize } from 'normalizr';
-import { Parser as NovelParser } from 'pixiv-novel-parser';
+import parseNovelText from '../helpers/novelTextParser';
 import Schemas from '../constants/schemas';
 
 function defaultEqualityCheck(currentVal, previousVal) {
@@ -503,59 +503,8 @@ export const makeGetNovelSeriesItems = () =>
 export const makeGetParsedNovelText = () =>
   createSelector([selectNovelText, getProps], (novelText, props) => {
     const novelId = props.novelId || props.navigation.state.params.novelId;
-    if (novelText[novelId] && novelText[novelId].text) {
-      // const parsedNovelText = NovelParser.parse(
-      //   `${novelText[novelId]
-      //     .text}[jump:2]blabla[[jumpuri:とある[[rb: 魔術 > まじゅつ]]の[[rb:禁書目録>インデックス]] > http://www.project-index.net/]]`,
-      // );
-      const parsedNovelText = NovelParser.parse(novelText[novelId].text);
-      const items = [];
-      let text = '';
-      parsedNovelText.forEach((p, index) => {
-        if (p.type === 'text') {
-          text += p.val;
-        } else if (p.type === 'tag') {
-          if (p.name === 'chapter') {
-            text += '<chapter>';
-            p.title.forEach(pp => {
-              if (pp.name === 'text') {
-                text += pp.val;
-              } else if (pp.name === 'rb') {
-                text += `${pp.rubyBase}(${pp.rubyText})`;
-              }
-            });
-            text += '</chapter>';
-          } else if (p.name === 'rb') {
-            text += `${p.rubyBase}(${p.rubyText})`;
-          } else if (p.name === 'jump') {
-            text += `<jump page=${p.pageNumber}>${p.pageNumber}ページへ</jump>`;
-          } else if (p.name === 'jumpuri') {
-            text += `<a href='${p.uri}'>`;
-            p.title.forEach(pp => {
-              if (pp.type === 'text') {
-                text += pp.val;
-              } else if (pp.type === 'rb') {
-                text += `${pp.rubyBase}(${pp.rubyText})`;
-              }
-            });
-            text += '</a>';
-          } else if (p.name === 'newpage') {
-            items.push(text);
-            text = '';
-          }
-        }
-        // if this is last item and no new page tag
-        if (
-          index === parsedNovelText.length - 1 &&
-          (!items.length || p.name !== 'newpage')
-        ) {
-          items.push(text);
-        }
-      });
-      return items;
-    }
     return novelText[novelId] && novelText[novelId].text
-      ? NovelParser.parse(novelText[novelId].text)
+      ? parseNovelText(novelText[novelId].text)
       : null;
   });
 
