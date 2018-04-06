bbb7c7a8532847c25653646e8d1e1b473e642926
alphasp <gmerudotcom@gmail.com>
2018-04-06 21:17:21 +0800

    upgrade redux-persist to v5.9.1, update codes to work with redux-persist v5

--- src/common/store/configureStore.js
+++ src/common/store/configureStore.js
@@ -1,13 +1,109 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 import invariant from 'redux-immutable-state-invariant';
-import { persistStore, autoRehydrate, createTransform } from 'redux-persist';
-import { REHYDRATE } from 'redux-persist/constants';
-import createActionBuffer from 'redux-action-buffer';
+import { persistStore, persistReducer, createTransform } from 'redux-persist';
+import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
+import getStoredStateMigrateV4 from 'redux-persist/lib/integration/getStoredStateMigrateV4';
 import applyAppStateListener from 'redux-enhancer-react-native-appstate';
 import { AsyncStorage } from 'react-native';
 import rootReducer from '../reducers';
 import rootSaga from '../sagas';
+
+const myTransform = createTransform(
+  (inboundState, key, state) => {
+    switch (key) {
+      case 'entities': {
+        const {
+          entities,
+          browsingHistoryIllusts,
+          browsingHistoryNovels,
+          muteUsers,
+        } = state;
+        const selectedUsersEntities = {};
+        const selectedIllustsEntities = browsingHistoryIllusts.items
+          .filter(id => entities.illusts[id])
+          .reduce((prev, id) => {
+            prev[id] = entities.illusts[id];
+            const userId = entities.illusts[id].user;
+            selectedUsersEntities[userId] = entities.users[userId];
+            return prev;
+          }, {});
+        const selectedNovelsEntities = browsingHistoryNovels.items
+          .filter(id => entities.novels[id])
+          .reduce((prev, id) => {
+            prev[id] = entities.novels[id];
+            const userId = entities.novels[id].user;
+            selectedUsersEntities[userId] = entities.users[userId];
+            return prev;
+          }, {});
+        const selectedUsersEntities2 = muteUsers.items
+          .filter(id => entities.users[id])
+          .reduce((prev, id) => {
+            prev[id] = entities.users[id];
+            return prev;
+          }, {});
+        const finalSelectedUsersEntities = {
+          ...selectedUsersEntities,
+          ...selectedUsersEntities2,
+        };
+        return {
+          ...inboundState,
+          illusts: selectedIllustsEntities,
+          novels: selectedNovelsEntities,
+          users: finalSelectedUsersEntities,
+        };
+      }
+      case 'browsingHistoryIllusts': {
+        const { entities, browsingHistoryIllusts } = state;
+        return {
+          ...inboundState,
+          items: browsingHistoryIllusts.items.filter(
+            id =>
+              entities.illusts[id] &&
+              entities.illusts[id].user &&
+              entities.users[entities.illusts[id].user],
+          ),
+        };
+      }
+      case 'browsingHistoryNovels': {
+        const { entities, browsingHistoryNovels } = state;
+        return {
+          ...inboundState,
+          items: browsingHistoryNovels.items.filter(
+            id =>
+              entities.novels[id] &&
+              entities.novels[id].user &&
+              entities.users[entities.novels[id].user],
+          ),
+        };
+      }
+      default:
+        return inboundState;
+    }
+  },
+  outboundState => outboundState,
+  {
+    whitelist: [
+      'entities',
+      'browsingHistoryIllusts',
+      'browsingHistoryNovels',
+      'muteUsers',
+    ],
+  },
+);
+
+const clearV4PersistedContents = () =>
+  AsyncStorage.getAllKeys((err, keys) => {
+    if (!err && keys && keys.length) {
+      const keyPrefix = 'reduxPersist:';
+      const v4PersistKeys = keys.filter(key => key.indexOf(keyPrefix) === 0);
+      if (v4PersistKeys.length) {
+        AsyncStorage.multiRemove(v4PersistKeys, () => Promise.resolve());
+      }
+      return Promise.resolve();
+    }
+    return Promise.resolve();
+  });
 
 export default function configureStore() {
   let enhancer;
@@ -17,109 +113,18 @@ export default function configureStore() {
       // eslint-disable-next-line no-underscore-dangle
       window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
     enhancer = composeEnhancers(
-      autoRehydrate({ log: true }),
       applyAppStateListener(),
-      applyMiddleware(
-        invariant(),
-        createActionBuffer(REHYDRATE),
-        sagaMiddleware,
-      ),
+      applyMiddleware(invariant(), sagaMiddleware),
       // devTools(),
     );
   } else {
     enhancer = compose(
-      autoRehydrate(),
       applyAppStateListener(),
-      applyMiddleware(createActionBuffer(REHYDRATE), sagaMiddleware),
+      applyMiddleware(sagaMiddleware),
     );
   }
-  const store = createStore(rootReducer, undefined, enhancer);
-  sagaMiddleware.run(rootSaga);
 
-  const myTransform = createTransform(
-    (inboundState, key) => {
-      switch (key) {
-        case 'entities': {
-          const {
-            entities,
-            browsingHistoryIllusts,
-            browsingHistoryNovels,
-            muteUsers,
-          } = store.getState();
-          const selectedUsersEntities = {};
-          const selectedIllustsEntities = browsingHistoryIllusts.items
-            .filter(id => entities.illusts[id])
-            .reduce((prev, id) => {
-              prev[id] = entities.illusts[id];
-              const userId = entities.illusts[id].user;
-              selectedUsersEntities[userId] = entities.users[userId];
-              return prev;
-            }, {});
-          const selectedNovelsEntities = browsingHistoryNovels.items
-            .filter(id => entities.novels[id])
-            .reduce((prev, id) => {
-              prev[id] = entities.novels[id];
-              const userId = entities.novels[id].user;
-              selectedUsersEntities[userId] = entities.users[userId];
-              return prev;
-            }, {});
-          const selectedUsersEntities2 = muteUsers.items
-            .filter(id => entities.users[id])
-            .reduce((prev, id) => {
-              prev[id] = entities.users[id];
-              return prev;
-            }, {});
-          const finalSelectedUsersEntities = {
-            ...selectedUsersEntities,
-            ...selectedUsersEntities2,
-          };
-          return {
-            ...inboundState,
-            illusts: selectedIllustsEntities,
-            novels: selectedNovelsEntities,
-            users: finalSelectedUsersEntities,
-          };
-        }
-        case 'browsingHistoryIllusts': {
-          const { entities, browsingHistoryIllusts } = store.getState();
-          return {
-            ...inboundState,
-            items: browsingHistoryIllusts.items.filter(
-              id =>
-                entities.illusts[id] &&
-                entities.illusts[id].user &&
-                entities.users[entities.illusts[id].user],
-            ),
-          };
-        }
-        case 'browsingHistoryNovels': {
-          const { entities, browsingHistoryNovels } = store.getState();
-          return {
-            ...inboundState,
-            items: browsingHistoryNovels.items.filter(
-              id =>
-                entities.novels[id] &&
-                entities.novels[id].user &&
-                entities.users[entities.novels[id].user],
-            ),
-          };
-        }
-        default:
-          return inboundState;
-      }
-    },
-    outboundState => outboundState,
-    {
-      whitelist: [
-        'entities',
-        'browsingHistoryIllusts',
-        'browsingHistoryNovels',
-        'muteUsers',
-      ],
-    },
-  );
-
-  persistStore(store, {
+  const v4Config = {
     whitelist: [
       'searchHistory',
       'browsingHistoryIllusts',
@@ -135,10 +140,42 @@ export default function configureStore() {
     ],
     storage: AsyncStorage,
     transforms: [myTransform],
-  });
+  };
+
+  const persistConfig = {
+    key: 'root',
+    stateReconciler: autoMergeLevel2,
+    whitelist: [
+      'searchHistory',
+      'browsingHistoryIllusts',
+      'browsingHistoryNovels',
+      'highlightTags',
+      'muteTags',
+      'muteUsers',
+      'saveImageSettings',
+      'novelSettings',
+      'entities',
+      'auth',
+      'i18n',
+      'temptest',
+    ],
+    storage: AsyncStorage,
+    transforms: [myTransform],
+    getStoredState: getStoredStateMigrateV4(v4Config),
+    debug: process.env.NODE_ENV !== 'production',
+  };
+  const persistedReducer = persistReducer(persistConfig, rootReducer);
+  const store = createStore(persistedReducer, undefined, enhancer);
+  sagaMiddleware.run(rootSaga);
+
+  const persistor = persistStore(store, undefined, clearV4PersistedContents);
   if (module.hot) {
-    module.hot.accept('../reducers', () => store.replaceReducer(rootReducer));
+    // eslint-disable-next-line global-require
+    const nextRootReducer = require('../reducers');
+    module.hot.accept(() => {
+      store.replaceReducer(persistReducer(persistConfig, nextRootReducer));
+    });
   }
 
-  return store;
+  return { store, persistor };
 }
--- src/screens/App/Root.js
+++ src/screens/App/Root.js
@@ -2,13 +2,15 @@
 import React, { Component } from 'react';
 import { AppRegistry } from 'react-native';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import App from './App';
 import { LocalizationProvider } from '../../components/Localization';
+import Loader from '../../components/Loader';
 import i18n from '../../common/helpers/i18n';
 import configureStore from '../../common/store/configureStore';
 // GLOBAL.XMLHttpRequest = GLOBAL.originalXMLHttpRequest || GLOBAL.XMLHttpRequest;
 
-const store = configureStore();
+const { store, persistor } = configureStore();
 
 if (process.env.NODE_ENV === 'production') {
   // eslint-disable-line no-undef
@@ -47,7 +49,9 @@ class Root extends Component {
     return (
       <Provider store={store}>
         <LocalizationProvider i18n={i18n}>
-          <App />
+          <PersistGate loading={<Loader />} persistor={persistor}>
+            <App />
+          </PersistGate>
         </LocalizationProvider>
       </Provider>
     );
