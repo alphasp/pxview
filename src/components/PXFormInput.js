fc04ff226fac54039015281e378aa67c5466235e
alphasp <gmerudotcom@gmail.com>
2018-07-25 21:43:29 +0800

    added underlineColorAndroid for PXFormInput as react native 0.56 change it default value to transparent

--- src/components/PXFormInput.js
+++ src/components/PXFormInput.js
@@ -39,6 +39,7 @@ class PXFormInput extends Component {
           onChangeText={this.handleOnChangeText}
           onBlur={this.handleOnBlur}
           value={value}
+          underlineColorAndroid="#009688"
         />
         {touched[name] &&
           errors[name] &&
@@ -49,38 +50,5 @@ class PXFormInput extends Component {
     );
   }
 }
-
-// const PXFormInput = props => {
-//   const {
-//     label,
-//     meta: { touched, dirty, error },
-//     labelStyle,
-//     inputStyle,
-//     errorTextStyle,
-//     input,
-//     ...restProps
-//   } = props;
-//   return (
-//     <View>
-//       <FormLabel labelStyle={labelStyle}>
-//         {label}
-//       </FormLabel>
-//       <FormInput
-//         inputStyle={inputStyle}
-//         {...input}
-//         {...restProps}
-//         onChangeText={text => {
-//           input.onChange(text);
-//         }}
-//         value={props.text || (dirty ? undefined : input.value)}
-//       />
-//       {touched &&
-//         error &&
-//         <FormValidationMessage>
-//           {error}
-//         </FormValidationMessage>}
-//     </View>
-//   );
-// };
 
 export default PXFormInput;
