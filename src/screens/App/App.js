1019d8a1db6a471cdc5f9f2ca525b4a17b510510
alphasp <gmerudotcom@gmail.com>
2020-03-29 19:16:21 +0900

    Update App.js to functional component with hooks

--- src/screens/App/App.js
+++ src/screens/App/App.js
@@ -1,12 +1,6 @@
-import React, { Component } from 'react';
-import {
-  View,
-  StyleSheet,
-  DeviceEventEmitter,
-  StatusBar,
-  Text,
-} from 'react-native';
-import { connect } from 'react-redux';
+import React, { useEffect, useRef } from 'react';
+import { View, StyleSheet, DeviceEventEmitter, StatusBar } from 'react-native';
+import { connect, useSelector } from 'react-redux';
 import {
   NavigationContainer,
   DefaultTheme as NavigationDefaultTheme,
@@ -22,12 +16,12 @@ import { MessageBar, MessageBarManager } from 'react-native-message-bar';
 import Toast, { DURATION } from 'react-native-easy-toast';
 import AppNavigator from '../../navigations/AppNavigator';
 import LoginNavigator from '../../navigations/LoginNavigator';
-import { connectLocalization } from '../../components/Localization';
+import { useLocalization } from '../../components/Localization';
 import Loader from '../../components/Loader';
 import ModalRoot from '../../containers/ModalRoot';
-import * as routeActionCreators from '../../common/actions/route';
 import { THEME_TYPES } from '../../common/constants';
 import { globalStyleVariables } from '../../styles';
+import usePrevious from '../../common/hooks/usePrevious';
 
 const styles = StyleSheet.create({
   container: {
@@ -35,138 +29,111 @@ const styles = StyleSheet.create({
   },
 });
 
-class App extends Component {
-  componentDidMount() {
-    MessageBarManager.registerMessageBar(this.messageBarAlert);
-    this.showToastListener = DeviceEventEmitter.addListener(
+const App = () => {
+  // const error = useSelector((state) => state.error);
+  const rehydrated = useSelector((state) => state.auth.rehydrated);
+  const user = useSelector((state) => state.auth.user);
+  const initialRouteName = useSelector(
+    (state) => state.initialScreenSettings.routeName,
+  );
+  const themeName = useSelector((state) => state.theme.name);
+
+  const messageBarAlertRef = useRef(null);
+  const toastRef = useRef(null);
+  const prevRehydrated = usePrevious(rehydrated);
+  const i18n = useLocalization();
+
+  useEffect(() => {
+    MessageBarManager.registerMessageBar(messageBarAlertRef.current);
+    return () => {
+      MessageBarManager.unregisterMessageBar();
+    };
+  }, []);
+  useEffect(() => {
+    const showToastListener = DeviceEventEmitter.addListener(
       'showToast',
       (text) => {
-        this.toast.show(text, DURATION.LENGTH_LONG);
+        toastRef.current.show(text, DURATION.LENGTH_LONG);
       },
     );
-    const { rehydrated } = this.props;
-    if (rehydrated) {
-      // call when reopen app after exit by back button on android
-      SplashScreen.hide();
-    }
-  }
+    return () => {
+      showToastListener.remove();
+    };
+  }, []);
 
-  componentWillReceiveProps(nextProps) {
-    const { rehydrated: prevRehydrated } = this.props;
-    const { rehydrated } = nextProps;
+  useEffect(() => {
     if (!prevRehydrated && rehydrated) {
       SplashScreen.hide();
     }
-  }
+  }, [prevRehydrated, rehydrated]);
 
-  componentWillUnmount() {
-    MessageBarManager.unregisterMessageBar();
-    this.showToastListener.remove();
-  }
-
-  render() {
-    const { rehydrated, user, i18n, themeName, initialRouteName } = this.props;
-    let renderComponent;
-    // const theme = {
-    //   ...selectedTheme,
-    //   fonts: {
-    //     ...selectedTheme.fonts,
-    //     regular: null, // use default font
-    //     medium: null,
-    //     light: null,
-    //     thin: null,
-    //   },
-    //   colors: {
-    //     ...selectedTheme.colors,
-    //     primary: globalStyleVariables.PRIMARY_COLOR,
-    //     activeTint: selectedTheme.dark
-    //       ? '#000000'
-    //       : globalStyleVariables.PRIMARY_COLOR,
-    //     headerBackground: selectedTheme.dark
-    //       ? '#1a1a1a'
-    //       : globalStyleVariables.PRIMARY_COLOR,
-    //     modalTitleBackground: selectedTheme.dark ? '#1a1a1a' : '#E9EBEE',
-    //   },
-    //   mode: 'exact',
-    // };
-    let theme;
-    const extraColorsConfig = {
-      primary: globalStyleVariables.PRIMARY_COLOR,
-      activeTint:
-        themeName === THEME_TYPES.DARK
-          ? '#000000'
-          : globalStyleVariables.PRIMARY_COLOR,
-      headerBackground:
-        themeName === THEME_TYPES.DARK
-          ? '#1a1a1a'
-          : globalStyleVariables.PRIMARY_COLOR,
-      modalTitleBackground:
-        themeName === THEME_TYPES.DARK ? '#1a1a1a' : '#E9EBEE',
+  let renderComponent;
+  let theme;
+  const extraColorsConfig = {
+    primary: globalStyleVariables.PRIMARY_COLOR,
+    activeTint:
+      themeName === THEME_TYPES.DARK
+        ? '#000000'
+        : globalStyleVariables.PRIMARY_COLOR,
+    headerBackground:
+      themeName === THEME_TYPES.DARK
+        ? '#1a1a1a'
+        : globalStyleVariables.PRIMARY_COLOR,
+    modalTitleBackground:
+      themeName === THEME_TYPES.DARK ? '#1a1a1a' : '#E9EBEE',
+  };
+  if (themeName === THEME_TYPES.DARK) {
+    theme = {
+      ...PaperDarkTheme,
+      ...NavigationDarkTheme,
+      colors: {
+        ...PaperDarkTheme.colors,
+        ...NavigationDarkTheme.colors,
+        ...extraColorsConfig,
+      },
     };
-    if (themeName === THEME_TYPES.DARK) {
-      theme = {
-        ...PaperDarkTheme,
-        ...NavigationDarkTheme,
-        colors: {
-          ...PaperDarkTheme.colors,
-          ...NavigationDarkTheme.colors,
-          ...extraColorsConfig,
-        },
-      };
-    } else {
-      theme = {
-        ...PaperDefaulTheme,
-        ...NavigationDefaultTheme,
-        colors: {
-          ...PaperDefaulTheme.colors,
-          ...NavigationDefaultTheme.colors,
-          ...extraColorsConfig,
-        },
-      };
-    }
-    if (!rehydrated) {
-      renderComponent = <Loader />;
-    } else if (user) {
-      renderComponent = (
-        <AppNavigator
-          // screenProps={{ i18n, theme }}
-          initialRouteName={initialRouteName}
-          uriPrefix={/^(?:https?:\/\/)?(?:www|touch)\.pixiv\.net\/|^pixiv:\/\//}
-        />
-      );
-    } else {
-      renderComponent = <LoginNavigator screenProps={{ i18n, theme }} />;
-    }
-    return (
-      <PaperProvider theme={theme}>
-        <NavigationContainer>
-          <View style={styles.container}>
-            <StatusBar
-              barStyle="light-content"
-              backgroundColor="rgba(0, 0, 0, 0.3)"
-              translucent
-              animated
-            />
-            {renderComponent}
-            <MessageBar ref={(ref) => (this.messageBarAlert = ref)} />
-            <Toast ref={(ref) => (this.toast = ref)} opacity={0.7} />
-            <ModalRoot />
-          </View>
-        </NavigationContainer>
-      </PaperProvider>
-    );
+  } else {
+    theme = {
+      ...PaperDefaulTheme,
+      ...NavigationDefaultTheme,
+      colors: {
+        ...PaperDefaulTheme.colors,
+        ...NavigationDefaultTheme.colors,
+        ...extraColorsConfig,
+      },
+    };
   }
-}
+  if (!rehydrated) {
+    renderComponent = <Loader />;
+  } else if (user) {
+    renderComponent = (
+      <AppNavigator
+        // screenProps={{ i18n, theme }}
+        initialRouteName={initialRouteName}
+        uriPrefix={/^(?:https?:\/\/)?(?:www|touch)\.pixiv\.net\/|^pixiv:\/\//}
+      />
+    );
+  } else {
+    renderComponent = <LoginNavigator screenProps={{ i18n, theme }} />;
+  }
+  return (
+    <PaperProvider theme={theme}>
+      <NavigationContainer>
+        <View style={styles.container}>
+          <StatusBar
+            barStyle="light-content"
+            backgroundColor="rgba(0, 0, 0, 0.3)"
+            translucent
+            animated
+          />
+          {renderComponent}
+          <MessageBar ref={messageBarAlertRef} />
+          <Toast ref={toastRef} opacity={0.7} />
+          <ModalRoot />
+        </View>
+      </NavigationContainer>
+    </PaperProvider>
+  );
+};
 
-export default connectLocalization(
-  connect(
-    (state) => ({
-      error: state.error,
-      rehydrated: state.auth.rehydrated,
-      user: state.auth.user,
-      initialRouteName: state.initialScreenSettings.routeName,
-      themeName: state.theme.name,
-    }),
-    routeActionCreators,
-  )(App),
-);
+export default App;
