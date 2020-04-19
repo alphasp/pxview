0129f8fdeffbb651e25dfb77eebcad81369c22f3
alphasp <gmerudotcom@gmail.com>
2020-04-20 00:45:32 +0900

    Fix missing SignUp page screen add workaround for createNativeStackNavigator safe area view

--- src/navigations/LoginNavigator.js
+++ src/navigations/LoginNavigator.js
@@ -1,14 +1,13 @@
 import React from 'react';
+import { View } from 'react-native';
 import { createNativeStackNavigator } from 'react-native-screens/native-stack';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import { useTheme } from 'react-native-paper';
 import Login from '../screens/Login/Login';
+import SignUp from '../screens/Login/SignUp';
 import PrivacyPolicy from '../screens/MyPage/PrivacyPolicy';
 import { useLocalization } from '../components/Localization';
-import {
-  globalStyles,
-  globalStyleVariables,
-  getThemedHeaderStyle,
-} from '../styles';
+import { globalStyleVariables, getThemedHeaderStyle } from '../styles';
 import { SCREENS } from '../common/constants';
 
 const Stack = createNativeStackNavigator();
@@ -18,31 +17,46 @@ const LoginNavigator = () => {
   const { i18n } = useLocalization();
   const headerStyle = getThemedHeaderStyle(theme);
   return (
-    <Stack.Navigator
-      screenOptions={{
-        headerStyle: {
-          backgroundColor: globalStyleVariables.HEADER_BACKGROUND_COLOR,
-        },
-        headerTintColor: globalStyleVariables.HEADER_TINT_COLOR,
-        headerBackTitle: null,
-      }}
+    <SafeAreaView
+      style={{ flex: 1, backgroundColor: theme.colors.headerBackground }}
     >
-      <Stack.Screen
-        name={SCREENS.Login}
-        component={Login}
-        options={{
-          headerShown: false,
-        }}
-      />
-      <Stack.Screen
-        name={SCREENS.PrivacyPolicy}
-        component={PrivacyPolicy}
-        options={{
-          title: i18n.privacyPolicy,
-          headerStyle,
-        }}
-      />
-    </Stack.Navigator>
+      <View style={{ flex: 1, backgroundColor: '#000' }}>
+        <Stack.Navigator
+          screenOptions={{
+            headerStyle: {
+              backgroundColor: globalStyleVariables.HEADER_BACKGROUND_COLOR,
+            },
+            headerTintColor: globalStyleVariables.HEADER_TINT_COLOR,
+            headerBackTitle: null,
+          }}
+        >
+          <Stack.Screen
+            name={SCREENS.Login}
+            component={Login}
+            options={{
+              headerShown: false,
+            }}
+          />
+          <Stack.Screen
+            name={SCREENS.SignUp}
+            component={SignUp}
+            options={{
+              title: i18n.signUp,
+              headerStyle,
+            }}
+          />
+          <Stack.Screen
+            name={SCREENS.PrivacyPolicy}
+            component={PrivacyPolicy}
+            options={{
+              title: i18n.privacyPolicy,
+              headerStyle,
+            }}
+          />
+        </Stack.Navigator>
+      </View>
+    </SafeAreaView>
   );
 };
+
 export default LoginNavigator;
